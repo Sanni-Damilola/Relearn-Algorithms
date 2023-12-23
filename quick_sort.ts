9e39e38d411aa2ef22852e7b439f3af44fa11899
Sanni-Damilola <sannifortune11@gmail.com>
2023-12-23 12:26:37 +0100

    added

--- quick_sort.ts
+++ quick_sort.ts
@@ -18,10 +18,8 @@ const setIn = (arr: number[], value: number): number[][] => {
 // console.log(setIn([7, 3, 0, 4], 5));
 
 //  MERGE SORT
-
-// console.time("")
-
-const merge = (arr1: number[], arr2: number[]) => {
+// Merge function for merging two sorted arrays
+const merge = (arr1: number[], arr2: number[]): number[] => {
   let res: number[] = [];
   let i: number = 0;
   let j: number = 0;
@@ -49,102 +47,52 @@ const merge = (arr1: number[], arr2: number[]) => {
   return res;
 };
 
-// const OurSort = (arr: number[]) => {
-//   if (arr.length <= 1) {
-//     return arr;
-//   }
+// Merge Sort implementation
+const mergeSort = (arr: number[]): number[] => {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-//   let mid: number = Math.floor(arr.length / 2);
-//   let left = OurSort(arr.slice(0, mid));
-//   let right = OurSort(arr.slice(mid));
+  const mid: number = Math.floor(arr.length / 2);
+  const left: number[] = mergeSort(arr.slice(0, mid));
+  const right: number[] = mergeSort(arr.slice(mid));
 
-//   const testData2: number[] = [];
+  return merge(left, right);
+};
 
-//   Array.apply(null, { length: 10000 }).map((el) => {
-//     if (el === undefined) {
-//       el = Math.floor(Math.random() * 100000);
-//       testData2.push(el);
-//     }
-//   });
+// Quick Sort implementation
+const quickSort = (arr: number[], start: number = 0, end: number = arr.length - 1): number[] => {
+  if (start < end) {
+    const pivotIdx: number = partition(arr, start, end);
+    quickSort(arr, start, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, end);
+  }
 
-//   return merge(left, right);
-// };
-// console.log(merge([1, 3, 0], [7, 2, 9]));
-// console.log(OurSort([1, 3, 0, 7, 2, 9, 15, 8, 12]));
-// console.log(merge(testData1));
+  return arr;
+};
 
-// const OurStart = (arr: number[], start: number=0, end: number = arr.length + 1) => {
-//     const swap = (arr: number[], i:number, j:number) => {
-//         let temp = arr[i];
-//         arr[j] = arr[j]
-//         arr[j] = temp
-//     };
+// Partition function for Quick Sort
+const partition = (arr: number[], start: number, end: number): number => {
+  const swap = (arr: number[], i: number, j: number) => {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  };
 
-//     let pivot = arr[start]
-//     let idx = start
+  let pivot = arr[start];
+  let idx = start;
 
-//     for(let i = 0; i < arr.length; i++){
-//         if(pivot > arr[i]){
-//             idx++
-//             swap(arr, idx, i)
-//         }
-//     }
-//     swap(arr, idx, start)
-//     return idx
-// }
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      idx++;
+      swap(arr, idx, i);
+    }
+  }
 
-// console.log("SWAP: ", OurStart([10, 1, 3, 0, 7, 2, 9, 15, 8, 12]))
-// console.timeLog("")
+  swap(arr, start, idx);
+  return idx;
+};
 
-// const OurQuickSort = (arr: number[], start: number = 0, end: number = arr.length -1) => {
-//     if(start < end){
-//         let pivotIDX = OurStart(arr, start, end);
-//         OurQuickSort(arr, start, pivotIDX -1)
-//         OurQuickSort(arr, pivotIDX + 1, end)
-//     }
-
-//     return arr
-// }
-
-// console.log("SWAP: ", OurStart)
-
-// const OurStart = (
-//   arr: number[],
-//   start: number = 0,
-//   end: number = arr.length + 1
-// ) => {
-//   const swap = (arr: number[], i: number, j: number) => {
-//     let temp = arr[i];
-//     arr[i] = arr[j];
-//     arr[j] = temp;
-//   };
-
-//   let pivot = arr[start];
-//   let idx = start;
-
-//   for (let i = start + 1; i < arr.length; i++) {
-//     if (pivot > arr[i]) {
-//       idx++;
-//       swap(arr, idx, i);
-//     }
-//   }
-
-//   swap(arr, start, idx);
-//   return idx;
-// };
-
-// const OurQuickSort = (
-//   arr: number[],
-//   start: number = 0,
-//   end: number = arr.length - 1
-// ) => {
-//   if (start < end) {
-//     let pivotIDX = OurStart(arr, start, end);
-//     OurQuickSort(arr, start, pivotIDX - 1);
-//     OurQuickSort(arr, pivotIDX + 1, end);
-//   }
-
-//   return arr;
-// };
-
-// console.log("QuickSort: ", OurQuickSort([1, 3, 7, 2, 9, 15, 8, 12]));
+// Example usage
+console.log("Merge Sort: ", mergeSort([1, 3, 0, 7, 2, 9, 15, 8, 12]));
+console.log("Quick Sort: ", quickSort([1, 3, 7, 2, 9, 15, 8, 12]));

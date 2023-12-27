fce4d45eb09bb52e6e1288c0fd73a4a01bbb086c
Sanni-Damilola <sannifortune11@gmail.com>
2023-12-27 13:01:08 +0100

    added

--- quick_sort.ts
+++ quick_sort.ts
@@ -1,5 +1,6 @@
-import { generateRandomArray } from "./utils";
+import { generateRandomArray3 } from "./utils";
 
+// SetIn function
 const setIn = (arr: number[], value: number): number[][] => {
   const res: number[][] = [];
   const hashtable: { [key: number]: number } = {};
@@ -17,9 +18,52 @@ const setIn = (arr: number[], value: number): number[][] => {
   return res;
 };
 
-// console.log(setIn([7, 3, 0, 4], 5));
+// Generate an array of 10,000 random numbers using generateRandomArray3
+const testDataSetIn: number[] = generateRandomArray3(10000);
 
-//  MERGE SORT
+// Measure time for setIn
+console.time("setIn");
+setIn(testDataSetIn, 5);
+console.timeEnd("setIn");
+
+// Merge Sort function
+const mergeSort = (arr: number[]): number[] => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid: number = Math.floor(arr.length / 2);
+  const left: number[] = mergeSort(arr.slice(0, mid));
+  const right: number[] = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+};
+
+// Measure time for Merge Sort
+console.time("Merge Sort");
+mergeSort(generateRandomArray3(10000000));
+console.timeEnd("Merge Sort");
+
+// Quick Sort function
+const quickSort = (
+  arr: number[],
+  start: number = 0,
+  end: number = arr.length - 1
+): number[] => {
+  if (start < end) {
+    const pivotIdx: number = partition(arr, start, end);
+    quickSort(arr, start, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, end);
+  }
+
+  return arr;
+};
+
+// Measure time for Quick Sort
+console.time("Quick Sort");
+quickSort(generateRandomArray3(10000000));
+console.timeEnd("Quick Sort");
+
 // Merge function for merging two sorted arrays
 const merge = (arr1: number[], arr2: number[]): number[] => {
   let res: number[] = [];
@@ -49,34 +93,6 @@ const merge = (arr1: number[], arr2: number[]): number[] => {
   return res;
 };
 
-// Merge Sort implementation
-const mergeSort = (arr: number[]): number[] => {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  const mid: number = Math.floor(arr.length / 2);
-  const left: number[] = mergeSort(arr.slice(0, mid));
-  const right: number[] = mergeSort(arr.slice(mid));
-
-  return merge(left, right);
-};
-
-// Quick Sort implementation
-const quickSort = (
-  arr: number[],
-  start: number = 0,
-  end: number = arr.length - 1
-): number[] => {
-  if (start < end) {
-    const pivotIdx: number = partition(arr, start, end);
-    quickSort(arr, start, pivotIdx - 1);
-    quickSort(arr, pivotIdx + 1, end);
-  }
-
-  return arr;
-};
-
 // Partition function for Quick Sort
 const partition = (arr: number[], start: number, end: number): number => {
   const swap = (arr: number[], i: number, j: number) => {
@@ -97,13 +113,4 @@ const partition = (arr: number[], start: number, end: number): number => {
 
   swap(arr, start, idx);
   return idx;
-}; // Measure time for Merge Sort
-
-console.time("Merge Sort");
-mergeSort(generateRandomArray(10000000));
-console.timeEnd("Merge Sort");
-
-// Measure time for Quick Sort
-console.time("Quick Sort");
-quickSort(generateRandomArray(10000000));
-console.timeEnd("Quick Sort");
+};

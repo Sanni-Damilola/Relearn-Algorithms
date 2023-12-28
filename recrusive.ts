49132bc6b6d688289b5ddd31e98cac3f067f4a35
Sanni-Damilola <sannifortune11@gmail.com>
2023-12-28 13:05:12 +0100

    added

--- recrusive.ts
+++ recrusive.ts
@@ -1,23 +1,24 @@
-// Recursive Method
-console.time("Recursive: ");
-const RecursiveMethod = (arr: number[], value: number) => {
+import { generateRandomArray3 } from "./utils";
+
+// Original Recursive Method
+console.time("Original Recursive");
+const originalRecursiveMethod = (arr: number[], value: number): boolean => {
   let midValue = Math.floor(arr.length / 2);
   let mid = arr[midValue];
 
   if (value === mid) {
     return true;
   } else if (value < mid && arr.length > 1) {
-    return RecursiveMethod(arr.slice(0, midValue), value);
+    return originalRecursiveMethod(arr.slice(0, midValue), value);
   } else if (value > mid && arr.length > 1) {
-    return RecursiveMethod(arr.slice(midValue, arr.length), value);
+    return originalRecursiveMethod(arr.slice(midValue, arr.length), value);
   }
   return false;
 };
-// console.log(RecursiveMethod([1, 4, 6, 8, 29], 1));
+console.timeEnd("Original Recursive");
 
-
-// Recursive Method
-const recursiveSearch = (arr: number[], value: number): boolean => {
+// Improved Recursive Method
+const improvedRecursiveSearch = (arr: number[], value: number): boolean => {
   // Base case: If the array is empty, the value is not found
   if (arr.length === 0) {
     return false;
@@ -31,19 +32,19 @@ const recursiveSearch = (arr: number[], value: number): boolean => {
     return true;
   } else if (value < midValue) {
     // Recursive case: Search in the left half of the array
-    return recursiveSearch(arr.slice(0, midIndex), value);
+    return improvedRecursiveSearch(arr.slice(0, midIndex), value);
   } else {
     // Recursive case: Search in the right half of the array
-    return recursiveSearch(arr.slice(midIndex + 1), value);
+    return improvedRecursiveSearch(arr.slice(midIndex + 1), value);
   }
 };
 
-// Test the recursive search
-const arrToSearch = [1, 4, 6, 8, 29];
-const searchValue = 1;
+// Test the improved recursive search with generateRandomArray3
+const arrToSearch = generateRandomArray3(10000);
+const searchValue = arrToSearch[Math.floor(Math.random() * arrToSearch.length)]; // Pick a random value
 
-console.time("Recursive Search");
-const isValueFound = recursiveSearch(arrToSearch, searchValue);
-console.timeEnd("Recursive Search");
+console.time("Improved Recursive");
+const isValueFound = improvedRecursiveSearch(arrToSearch, searchValue);
+console.timeEnd("Improved Recursive");
 
 console.log(`Is ${searchValue} found? ${isValueFound}`);
